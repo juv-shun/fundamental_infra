f1e64811ce1a1b700c227c432a095eda26f6a039
Shun Fukusumi <shun.fukusumi@gmail.com>
2020-08-18 19:40:08 +0900

    bastionにPowerUserAccessを付与

--- bastion/ec2.tf
+++ bastion/ec2.tf
@@ -22,7 +22,6 @@ EOF
 resource "aws_iam_instance_profile" "bastion_profile" {
   name = "bastion"
   role = aws_iam_role.bastion_role.name
-
 }
 
 resource "aws_iam_role" "bastion_role" {
@@ -42,4 +41,9 @@ resource "aws_iam_role" "bastion_role" {
   ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy_attachment" "bastion-attach-ro" {
+  role       = aws_iam_role.bastion_role.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
 }
