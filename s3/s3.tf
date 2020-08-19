ebf162cad3dcea1d8afb6aa2ada36df8388ecd5d
Shun Fukusumi <shun.fukusumi@gmail.com>
2020-08-19 09:05:29 +0900

    アクセスログのs3の設定を追加

--- s3/s3.tf
+++ s3/s3.tf
@@ -10,3 +10,29 @@ resource "aws_s3_bucket" "access_logs" {
     }
   }
 }
+
+resource "aws_s3_bucket_policy" "access_logs" {
+  bucket = aws_s3_bucket.access_logs.id
+  policy = data.aws_iam_policy_document.access_logs.json
+}
+
+resource "aws_s3_bucket_public_access_block" "access_logs" {
+  bucket                  = aws_s3_bucket.access_logs.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "access_logs" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.access_logs.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
