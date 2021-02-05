83c8a9774b5cc9dffe36f75535c7a8f29f71a68d
Shun Fukusumi <shun.fukusumi@gmail.com>
2021-02-05 21:36:11 +0900

    VPC terraformを改修

--- network/nat_gateway.tf
+++ network/nat_gateway.tf
@@ -12,48 +12,6 @@
 # }
 
 # #####################################
-# # Routes Table Settings
-# #####################################
-# resource "aws_route_table" "nat_route_az1" {
-#   vpc_id = aws_vpc.main.id
-
-#   route {
-#     cidr_block     = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.nat_gw_az1.id
-#   }
-
-#   tags = {
-#     Name = "[${var.network_name}] nat_gw_az1"
-#   }
-# }
-
-# resource "aws_route_table" "nat_route_az2" {
-#   vpc_id = aws_vpc.main.id
-
-#   route {
-#     cidr_block     = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.nat_gw_az2.id
-#   }
-
-#   tags = {
-#     Name = "[${var.network_name}] nat_gw_az2"
-#   }
-# }
-
-# #####################################
-# # Routes Association Settings
-# #####################################
-# resource "aws_route_table_association" "rt_association_az1" {
-#   subnet_id      = "${aws_subnet.private_subnet_az1.id}"
-#   route_table_id = "${aws_route_table.nat_route_az1.id}"
-# }
-
-# resource "aws_route_table_association" "rt_association_az2" {
-#   subnet_id      = "${aws_subnet.private_subnet_az2.id}"
-#   route_table_id = "${aws_route_table.nat_route_az2.id}"
-# }
-
-# #####################################
 # # Elastic IP for NAT Gateway
 # #####################################
 # resource "aws_eip" "gw_eip_az1" {
--- network/vpc.tf
+++ network/vpc.tf
@@ -83,7 +83,33 @@ resource "aws_route_table" "igw_route" {
     gateway_id = aws_internet_gateway.i_gw.id
   }
   tags = {
-    Name = var.network_name
+    Name = "${var.network_name}_igw"
+  }
+}
+
+resource "aws_route_table" "nat_route_az1" {
+  vpc_id = aws_vpc.main.id
+
+  # route {
+  #   cidr_block     = "0.0.0.0/0"
+  #   nat_gateway_id = aws_nat_gateway.nat_gw_az1.id
+  # }
+
+  tags = {
+    Name = "${var.network_name}_natgw_az1"
+  }
+}
+
+resource "aws_route_table" "nat_route_az2" {
+  vpc_id = aws_vpc.main.id
+
+  # route {
+  #   cidr_block     = "0.0.0.0/0"
+  #   nat_gateway_id = aws_nat_gateway.nat_gw_az2.id
+  # }
+
+  tags = {
+    Name = "${var.network_name}_natgw_az2"
   }
 }
 
@@ -98,4 +124,14 @@ resource "aws_route_table_association" "i_gw_association_az1" {
 resource "aws_route_table_association" "i_gw_association_az2" {
   subnet_id      = aws_subnet.public_subnet_az2.id
   route_table_id = aws_route_table.igw_route.id
+}
+
+resource "aws_route_table_association" "nat_association_az1" {
+  subnet_id      = aws_subnet.private_subnet_az1.id
+  route_table_id = aws_route_table.nat_route_az1.id
+}
+
+resource "aws_route_table_association" "rt_association_az2" {
+  subnet_id      = aws_subnet.private_subnet_az2.id
+  route_table_id = aws_route_table.nat_route_az2.id
 }
