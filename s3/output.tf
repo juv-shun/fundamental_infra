c91a95112d334e6c935a61b42a4364a73cbc71f8
Shun Fukusumi <shun.fukusumi@gmail.com>
2021-02-06 23:42:23 +0900

    outputにiam policyを追加

--- s3/output.tf
+++ s3/output.tf
@@ -1,6 +1,9 @@
 output "fundamental_bucket" {
   value = {
-    "access_logs"      = aws_s3_bucket.access_logs.id
-    "application_logs" = aws_s3_bucket.application_logs.id
+    "access_logs" = aws_s3_bucket.access_logs.id
+    "application_logs" = {
+      "bucket_name"       = aws_s3_bucket.application_logs.id
+      "access_policy_arn" = aws_iam_policy.put_application_log_bucket_policy.arn
+    }
   }
 }
