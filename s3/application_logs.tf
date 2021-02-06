5235fdd5e4414d214a393d41bce654a68a7ede41
Shun Fukusumi <shun.fukusumi@gmail.com>
2021-02-06 23:37:25 +0900

    アプリケーションログバケットにPUTできるポリシーを作成

--- s3/application_logs.tf
+++ s3/application_logs.tf
@@ -17,3 +17,28 @@ resource "aws_s3_bucket_public_access_block" "application_logs" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_iam_policy" "put_application_log_bucket_policy" {
+  name = "${var.account_name}-put_application_log_bucket_policy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowAccessToS3ApplicationLogBucket",
+            "Effect": "Allow",
+            "Action": [
+              "s3:ListObject",
+              "s3:GetObject",
+              "s3:PutObject",
+              "s3:DeleteObject"
+            ],
+            "Resource": [
+               "arn:aws:s3:::${aws_s3_bucket.application_logs.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+}
