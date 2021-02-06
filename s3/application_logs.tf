1e69016285db3e867f4c940146d58bd5cc160c62
Shun Fukusumi <shun.fukusumi@gmail.com>
2021-02-06 23:30:33 +0900

    application log用のs3のバケットポリシーを削除

--- s3/application_logs.tf
+++ s3/application_logs.tf
@@ -10,29 +10,10 @@ resource "aws_s3_bucket" "application_logs" {
     }
   }
 }
-
-resource "aws_s3_bucket_policy" "application_logs" {
-  bucket = aws_s3_bucket.application_logs.id
-  policy = data.aws_iam_policy_document.application_logs.json
-}
-
 resource "aws_s3_bucket_public_access_block" "application_logs" {
   bucket                  = aws_s3_bucket.application_logs.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-data "aws_iam_policy_document" "application_logs" {
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:PutObject"]
-    resources = ["arn:aws:s3:::${aws_s3_bucket.application_logs.id}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["582318560864"]
-    }
-  }
 }
